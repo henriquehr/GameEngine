e11358f760ad6b9b291e84003c010182b7f68399
Henrique A. Richter <henrique.a.richter@gmail.com>
2023-07-23 20:44:23 -0300

    Add staging buffer

--- src/model.cpp
+++ src/model.cpp
@@ -22,14 +22,25 @@ void Model::createVertexBuffer(const std::vector<Vertex> &vertices) {
 
     VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
 
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+
     // VK_MEMORY_PROPERTY_HOST_COHERENT_BIT automatic update changes in the GPU
-    device.createBuffer(bufferSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
-                        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, vertexBuffer, vertexBufferMemory);
+    device.createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
 
     void *data;
-    vkMapMemory(device.getDevice(), vertexBufferMemory, 0, bufferSize, 0, &data);
+    vkMapMemory(device.getDevice(), stagingBufferMemory, 0, bufferSize, 0, &data);
     memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
-    vkUnmapMemory(device.getDevice(), vertexBufferMemory);
+    vkUnmapMemory(device.getDevice(), stagingBufferMemory);
+
+    device.createBuffer(bufferSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+                        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, vertexBuffer, vertexBufferMemory);
+
+    device.copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+
+    vkDestroyBuffer(device.getDevice(), stagingBuffer, nullptr);
+    vkFreeMemory(device.getDevice(), stagingBufferMemory, nullptr);
 }
 
 void Model::createIndexBuffer(const std::vector<uint32_t> &indices) {
@@ -42,14 +53,25 @@ void Model::createIndexBuffer(const std::vector<uint32_t> &indices) {
 
     VkDeviceSize bufferSize = sizeof(indices[0]) * indexCount;
 
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+
     // VK_MEMORY_PROPERTY_HOST_COHERENT_BIT automatic update changes in the GPU
-    device.createBuffer(bufferSize, VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
-                        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, indexBuffer, indexBufferMemory);
+    device.createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
 
     void *data;
-    vkMapMemory(device.getDevice(), indexBufferMemory, 0, bufferSize, 0, &data);
+    vkMapMemory(device.getDevice(), stagingBufferMemory, 0, bufferSize, 0, &data);
     memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
-    vkUnmapMemory(device.getDevice(), indexBufferMemory);
+    vkUnmapMemory(device.getDevice(), stagingBufferMemory);
+
+    device.createBuffer(bufferSize, VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+                        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+
+    device.copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+    vkDestroyBuffer(device.getDevice(), stagingBuffer, nullptr);
+    vkFreeMemory(device.getDevice(), stagingBufferMemory, nullptr);
 }
 
 void Model::bind(VkCommandBuffer commandBuffer) {
